07a88c31deb4df8f4ee8244a54784c69e2d84238
Diego Griep <fale@diegogriep.com.br>
2020-10-19 18:56:49 -0300

    Handling arrow function this keyword.

--- src/extra/debounce.ts
+++ src/extra/debounce.ts
@@ -2,19 +2,20 @@ const debounce = (
   fn: Function,
   wait: number = 100,
   immediate: boolean = false
-) => (...args: any[]) => {
-  const obj = this;
-  let timeout;
+) =>
+  function(...args: any[]) {
+    const obj = this;
+    let timeout;
 
-  const delayed = () => {
-    if (!immediate) fn.apply(obj, args);
-    timeout = null;
+    const delayed = () => {
+      if (!immediate) fn.apply(obj, args);
+      timeout = null;
+    };
+
+    if (timeout) clearTimeout(timeout);
+    else if (immediate) fn.apply(obj, args);
+
+    timeout = setTimeout(delayed, wait);
   };
-
-  if (timeout) clearTimeout(timeout);
-  else if (immediate) fn.apply(obj, args);
-
-  timeout = setTimeout(delayed, wait);
-};
 
 export default debounce;
